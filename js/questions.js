2fa5747b5faa440721ae82cd9e3e45d7858377f3
mrcrazycola <80262802+mrcrazycola@users.noreply.github.com>
2022-07-25 19:17:14 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Totally how many books are present in Bible?",
+    answer: "66",
+    options: [
+      "62",
+      "66",
+      "65",
+      "67"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who is the authour of Judges?",
+    answer: "Samuel",
+    options: [
+      "Samuel",
+      "Joshua",
+      "Jeremiah",
+      "Ezra"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How long was Noah on the boat?",
+    answer: "about 1 year",
+    options: [
+      "about 11 months",
+      "about 2 years",
+      "about 10 months",
+      "about 1 year"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The New Testament was originally written in?",
+    answer: "Greek",
+    options: [
+      "Latin",
+      "Greek",
+      "Hebrew",
+      "None of these"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The Bible has over____authors",
+    answer: "40",
+    options: [
+      "27",
+      "53",
+      "40",
+      "33"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
